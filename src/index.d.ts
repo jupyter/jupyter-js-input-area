c99a8f3641baef25598d86872f1ca05b8233317c
Steven Silvester <steven.silvester@ieee.org>
2015-11-03 19:27:11 -0600

    Update the changed interface

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,24 +1,48 @@
 
+
+/**
+ * The arguments object emitted with the `stateChanged` signal.
+ */
+export
+interface IChangedArgs<T> {
+  name: string,
+  oldValue: T;
+  newValue: T;
+}
+
+
 export 
 interface IInputAreaViewModel {
-    textEditor: ITextEditorViewModel;
 
-    rendered: boolean; // whether to render the input area (or providing an editable input area)
-    collapsed: boolean;
+  /**
+   * A signal emitted when state of the input area changes.
+   */
+  stateChanged: ISignal<IInputAreaViewModel, IChangedArgs<any>>;
 
-    // TODO: Brian is going through the code to make sure we capture the right
-    // information in promptNumber and executionCount
-    promptNumber: number;
-    executionCount: number;
-    //think about storing dates for both of the above (and timestamp?).
+  textEditor: ITextEditorViewModel;
+
+  rendered: boolean; // whether to render the input area (or providing an editable input area)
+  collapsed: boolean;
+
+  // TODO: Brian is going through the code to make sure we capture the right
+  // information in promptNumber and executionCount
+  promptNumber: number;
+  executionCount: number;
+  //think about storing dates for both of the above (and timestamp?).
 }
 
 
 export 
 interface ITextEditorViewModel {
-    text: string;
-    // Eventually add an IObservableString or some other rich object
-    mimetype: string;
-    lineNumbers: boolean;
-    scrolled: boolean; // if true, fixed-height editor; if false, auto-expanding editor
+
+  /**
+   * A signal emitted when state of the text area changes.
+   */
+  stateChanged: ISignal<ITextEditorViewModel, IChangedArgs<any>>;
+
+  text: string;
+  // Eventually add an IObservableString or some other rich object
+  mimetype: string;
+  lineNumbers: boolean;
+  scrolled: boolean; // if true, fixed-height editor; if false, auto-expanding editor
 }
