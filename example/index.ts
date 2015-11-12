ca47fbbb82d602ccea683eb6914167b76a965aa4
Jason Grout <jgrout6@bloomberg.net>
2015-11-12 12:21:50 -0800

    Factor out code in the example.

--- example/index.ts
+++ example/index.ts
@@ -8,19 +8,17 @@ import {
   CodeMirrorWidget, InputAreaViewModel, TextEditorViewModel
 } from '../lib/index';
 
-function main(): void {
-  let textModel = new TextEditorViewModel();
-  textModel.text = `def f(n):
+let initialCode = `def f(n):
     for i in range(n):
         print(i)
-  `
-  textModel.mimetype = 'text/x-python'
-  textModel.lineNumbers = true;
-/*  let inputVModel = new InputAreaViewModel();
-  inputVModel.textEditor = textModel;
-  */
+`;
 
-  var c = new CodeMirrorWidget(textModel);
+function main(): void {
+  let textModel = new TextEditorViewModel();
+  textModel.text = initialCode;
+  textModel.mimetype = 'text/x-python';
+  textModel.lineNumbers = true;
+  let c = new CodeMirrorWidget(textModel);
   Widget.attach(c, document.body);
 }
 
